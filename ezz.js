4a9538d24d3c46e6438fcf5b1e770f8f0e63f7de
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-07 11:13:49 +0300

    Update ezz.js

--- ezz.js
+++ ezz.js
@@ -65,4 +65,43 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 }
 
 });
+
+
+  
+
+  client.on("message",  message => {
+
+         let args = message.content.split(' ').slice(1);
+
+    if(message.content.startsWith('3znickname')) {
+
+        if (!message.member.hasPermission("MANAGE_NICKNAMES")) {
+
+            message.channel.send("حط الاسم")
+
+        } else {
+
+            if (!message.guild.member(client.user).hasPermission('MANAGE_NICKNAMES')) return message.reply(' ❌البوت ما عنده خاصية MANAGE_NICKNAMES.').catch(console.error);
+
+            let changenick = message.mentions.users.first();
+
+            let username = args.slice(1).join(' ')
+
+            if (username.length < 1) return message.reply('حط الاسم').catch(console.error);
+
+            if (message.mentions.users.size < 1) return message.author.send('You must mention a user to change their nickname. ❌').catch(console.error);
+
+            message.guild.member(changenick.id).setNickname(username);
+
+            message.channel.send("تم تغيير الاسم الى: " + changenick + "")
+
+        }
+
+    }});
+
+  
+
+ 
+
+   
 client.login(process.env.BOT_TOKEN);  
